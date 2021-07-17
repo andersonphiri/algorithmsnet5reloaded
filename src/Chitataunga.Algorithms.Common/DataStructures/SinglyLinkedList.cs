414b69f1a7f6d09719186bf00c420254da0b259a
Anderson Phiri <andersonp@laxtongroup.com>
2021-07-17 22:32:21 +0800

    singly linked list

---
+++ src/Chitataunga.Algorithms.Common/DataStructures/SinglyLinkedList.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chitataunga.Algorithms.Common.DataStructures
+{
+    public class SinglyLinkedList<TType> : IEnumerable<TType>
+    {
+
+        private Node first;
+        private Node last;
+        private int N = 0;
+
+        public SinglyLinkedList()
+        {
+            first = new();
+            last = new Node() {Next = null};
+            first.Next = last;
+
+        }
+
+        public bool IsEmpty() => N == 0;
+        public TType DeleteAtStart()
+        {
+            Node oldLast = first;
+            first = first.Next;
+            N--;
+            return oldLast.Data;
+        }
+        public void InsertAtBeginning(TType data)
+        {
+            Node oldfirst = first;
+            first = new Node(data) {Next = oldfirst};
+            N++;
+            //check();
+        }
+        public void InsertAtEnd(TType data)
+        {
+            Node oldLast = last;
+            last = new(data) {Next = null};
+            if (IsEmpty())
+            {
+                first = last;
+            }
+            else
+            {
+                oldLast.Next = last;
+            }
+           
+
+            N++;
+            //check();
+
+        }
+
+        private class  Node
+        {
+            public Node()
+            {
+                
+            }
+            public Node(TType data)
+            {
+                Data = data;
+            }
+            public TType Data { get; set; }
+            public Node Next { get; set; }
+        }
+
+        private class SinglyLinkedListEnumerator : IEnumerator<TType>
+        {
+            private readonly SinglyLinkedList<TType> _list;
+            private bool firstCall = true;
+            public SinglyLinkedListEnumerator(SinglyLinkedList<TType> list)
+            {
+                _list = list;
+            }
+
+            private Node current = null;
+            public bool MoveNext()
+            {
+                if (firstCall)
+                {
+                    current = _list.first;
+                    firstCall = false;
+                    return current != null;
+                }
+
+                if (current != null)
+                {
+                    current = current.Next;
+                    return current != null;
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                current = null;
+                firstCall = true;
+            }
+
+            public TType Current
+            {
+                get
+                {
+                    if (current is null)
+                        throw new InvalidOperationException("This is past the end of the collection");
+                    return current.Data;
+                }
+            }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+                
+            }
+        }
+
+        // check internal invariants
+        private bool check()
+        {
+
+            // check a few properties of instance variable 'first'
+            if (N < 0)
+            {
+                return false;
+            }
+            if (N == 0)
+            {
+                if (first != null) return false;
+            }
+            else if (N == 1)
+            {
+                if (first == null) return false;
+                if (first.Next != null) return false;
+            }
+            else
+            {
+                if (first == null) return false;
+                if (first.Next == null) return false;
+            }
+
+            // check internal consistency of instance variable N
+            int numberOfNodes = 0;
+            for (Node x = first; x != null && numberOfNodes <= N; x = x.Next)
+            {
+                numberOfNodes++;
+            }
+            if (numberOfNodes != N) return false;
+
+            return true;
+        }
+
+        public IEnumerator<TType> GetEnumerator()
+        {
+            return new SinglyLinkedListEnumerator( this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ src/Chitataunga.Algorithms.Searching/BinarySearchTree.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chitataunga.Algorithms.Searching
+{
+    public class BinarySearchTree<TKeyType, TValueType> // : IEnumerable<TValueType>
+
+        where TKeyType : IComparable<TKeyType>
+    {
+        private Node root;
+
+        /// <summary>
+        /// Comparison time = 1 + Depth of the node
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Put(TKeyType key, TValueType value)
+        {
+            root = PutUtil(root, key, value);
+        }
+
+        private Node PutUtil(Node x, TKeyType key, TValueType value)
+        {
+            if (x is null) return new Node(key, value);
+            int comp = key.CompareTo(x.Key);
+            if (comp < 0) x.Left = PutUtil(x.Left, key, value); // go left
+            else if (comp > 0) PutUtil(x.Right, key, value); // go right
+            else x.Value = value; // update
+            x.Count = 1 + Size(x.Left) + Size(x.Right);
+            return x;
+        }
+
+        /// <summary>
+        /// Running time = 1 + Depth of the node
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+
+        public TValueType Get(TKeyType key)
+        {
+            Node x = root;
+            while (x is not null)
+            {
+                int comp = key.CompareTo(x.Key);
+                if (comp < 0) x = x.Left;
+                else if (comp > 0) x = x.Right;
+                else return x.Value;
+            }
+            return default;
+        }
+
+        #region Floor, Ceiling and Rank of a key
+
+        public TKeyType Floor(TKeyType key)
+        {
+            Node x = Floor(root, key);
+            if (x is null) return default;
+            return x.Key;
+        }
+        private Node Floor(Node x, TKeyType key)
+        {
+            if (x is null) return null;
+            int comp = key.CompareTo(x.Key);
+            if (comp == 0) return x;
+            if (comp < 0) return Floor(x.Left, key);
+            // then search to the right in the left side
+            Node t = Floor(x.Right, key);
+            if (t != null) return t;
+            return x;
+        }
+
+        /// <summary>
+        /// The key just greater than key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public TKeyType Ceiling(TKeyType key)
+        {
+            if (key == null) throw new ArgumentNullException("argument to Ceiling() is null"); 
+            if (IsEmpty()) throw new InvalidOperationException("called Ceiling() with empty symbol table");
+            Node x = Ceiling(root, key);
+            if (x == null) throw new KeyNotFoundException("ceiling key does not exist");
+            else return x.Key;
+        }
+
+        private Node Ceiling(Node x, TKeyType key)
+        {
+            if (x == null) return null;
+            int cmp = key.CompareTo(x.Key);
+            if (cmp == 0) return x;
+            if (cmp < 0)
+            {
+                Node t = Ceiling(x.Left, key);
+                if (t != null) return t;
+                return x;
+            }
+            return Ceiling(x.Right, key);
+        }
+        /// <summary>
+        /// How many keys < k
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public int Rank(TKeyType key)
+        {
+            return Rank(key, root);
+        }
+        private int Rank(TKeyType key, Node x)
+        {
+            if (x is null) return 0;
+            int comp = key.CompareTo(x.Key);
+            if (comp < 0) return Rank(key, x.Left);
+            else if (comp > 0) return 1 + Size(x.Left) + Rank(key, x.Right);
+            else
+            {
+                return Size(x.Left);
+            }
+        }
+
+        #endregion
+
+        #region Size
+
+        public int Count
+        {
+            get =>  Size(root);
+        }
+
+        private int Size(Node x) => x?.Count ?? 0;
+
+        #endregion
+
+        #region Deleting
+
+        public void DeleteMinimum()
+        {
+            root = DeleteMinimum(root);
+        }
+        public void DeleteMaximum()
+        {
+            root = DeleteMaximum(root);
+        }
+        /// <summary>
+        /// uses hibbard implementation
+        /// </summary>
+        /// <param name="key"></param>
+        public void Delete(TKeyType key) => root = Delete(root, key);
+        private Node DeleteMinimum(Node x)
+        {
+            if (x.Left is null) return x.Right;
+            x.Left = DeleteMinimum(x.Left);
+            x.Count = 1 + Size(x.Left) + Size(x.Right);
+            return x;
+        }
+        private Node DeleteMaximum(Node x)
+        {
+            if (x.Right is null) return x.Left;
+            x.Left = DeleteMaximum(x.Left);
+            x.Count = 1 + Size(x.Left) + Size(x.Right);
+            return x;
+        }
+        /// <summary>
+        /// Uses Hibbard implementation
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private Node Delete(Node x, TKeyType key)
+        {
+            if (x is null) return null;
+            int cmp = key.CompareTo(x.Key);
+            if (cmp < 0) x.Left = Delete(x.Left, key);
+            else if (cmp > 0) x.Right = Delete(x.Right, key);
+            else
+            {
+                if (x.Right is null) return x.Left;
+                Node t = x;
+                x = MinNode(t.Right);
+                x.Right = DeleteMinimum(t.Right);
+                x.Left = t.Left;
+            }
+
+            x.Count = Size(x.Left) + Size(x.Right) + 1;
+            return x;
+        }
+
+
+
+        public bool IsEmpty() => Count == 0;
+
+        public TKeyType MinKey
+        {
+            get
+            {
+                if (IsEmpty()) throw new InvalidOperationException("Tree is empty");
+                return MinNode(root).Key;
+            }
+        }
+
+        private Node MinNode(Node x)
+        {
+            if (x.Left is null) return x;
+            return MinNode(x.Left);
+        }
+        #endregion
+
+        private class Node
+        {
+            public TKeyType Key;
+            public TValueType Value;
+            public Node Left;
+            public Node Right;
+            public int Count;
+
+            public Node(TKeyType key, TValueType value)
+            {
+                Key = key;
+                Value = value;
+            }
+
+        }
+    }
+}

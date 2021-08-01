63397187d0fce36de56fcd9d5a1a6b9117733149
Anderson Phiri <phirianderson23@gmail.com>
2021-08-02 01:48:53 +0800

    intro graph api
    intro graph setup

---
+++ src/Chitataunga.Algorithms.Graph/Extensions/GraphExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chitataunga.Algorithms.Graph.Extensions
+{
+    public static class GraphExtensions
+    {
+        /// <summary>
+        /// computes the degree of vertex v from graph  g
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static int Degree(this Graph g, int v)
+        {
+            int degree = 0;
+            foreach (int w in g.Adj(v))
+            {
+                degree++;
+            }
+            return degree;
+        }
+
+        public static int MaxDegree(this Graph g)
+        {
+            int max = 0;
+            for (int v = 0; v <  g.V(); v++)
+            {
+                int temp = g.Degree(v);
+                if (temp > max) max = temp;
+            }
+            return max;
+        }
+        public static int AverageDegree(this Graph g)
+        {
+            return 2 * g.E() / g.V();
+        }
+        public static int NumberOfSelfLoops(this Graph g)
+        {
+            int count = 0;
+            for (int v = 0; v < g.V(); v++)
+            {
+                foreach (int w in g.Adj(v))
+                {
+                    if (v == w) count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ src/Chitataunga.Algorithms.Graph/Graph.cs
@@ -0,0 +1,66 @@
+﻿using Chitataunga.Algorithms.Common.DataStructures;
+using System;
+using System.Collections.Generic;
+
+namespace Chitataunga.Algorithms.Graph
+{
+    /// <summary>
+    /// Undirected graph
+    /// </summary>
+    public class Graph
+    {
+        private readonly int _V;
+        private int _E;
+        private RandomBag<int>[] _adj; // adjascency lists
+        public Graph(int v)
+        {
+            _V = v;
+            _E = 0;
+            _adj = new RandomBag<int>[_V];
+            for(int _v = 0; _v < _V; _v++)
+            {
+                _adj[v] = new RandomBag<int>();
+            }
+        }
+        public Graph(int v, int e)
+        {
+            _V = v;
+            _E = e;
+            _adj = new RandomBag<int>[_V];
+            for (int _v = 0; _v < _V; _v++)
+            {
+                _adj[v] = new RandomBag<int>();
+            }
+        }
+
+        public int E() => _E;
+        public int V() => _V;
+
+        public void AddEdge(int v, int w)
+        {
+            _adj[v].Add(w);
+
+            _adj[w].Add(v);
+            _E++;
+        }
+
+        public IEnumerable<int>  Adj(int v)
+        {
+            return _adj[v];
+        }
+
+        public override string ToString()
+        {
+
+            string s = _V + " vertices, " + _E + " edges\n";
+            for (int v = 0; v < _V; v++)
+            {
+                s += v + ": ";
+                foreach (int w in this.Adj(v))
+                    s += w + " ";
+                s += "\n";
+            }
+            return s;
+        }
+    }
+}
